eb9470623e88f0c103cdc45d5f53691ea85a43c9
jem <michael.jerger@meissa-gmbh.de>
2019-03-01 13:18:10 +0100

    work on letsencrypt

--- main/resources/setup.sh
+++ main/resources/setup.sh
@@ -1,18 +1,46 @@
-ssh user@159.69.207.106 -L 8001:localhost:8001
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "159.69.207.106"
+ssh `(whoami)`@159.69.207.106 -L 8001:localhost:8001
 
 sudo -i
-snap install microk8s --classic
-snap alias microk8s.kubectl kubectl
-microk8s.start
-microk8s.enable dns dashboard storage ingress metrics-server
-nano /var/snap/microk8s/current/args/kube-apiserver
+
+#nano /var/snap/microk8s/current/args/kube-apiserver
 # replace "–authorization-mode=AlwaysAllow" with "–authorization-mode=RBAC"
 # in line 5
-microk8s.stop
-microk8s.start
 nano dashboard-admin.yml
 # Paste yml from resources
-kubectl create -f dashboard-admin.yml
+
+# install cert issuers
+mkdir cert_manager
+#nano cert_manager/letsencryp_staging_issuer.yaml
+# Paste yml from resources
+nano cert_manager/letsencryp_prod_issuer.yaml
+# Paste yml from resources
+#nano cert_manager/selfsigned_issuer.yaml
+# Paste yml from resources
+nano cert_manager/ca_issuer.yaml
+# Paste yml from resources
+
+nano apple.yml
+# Paste yml from resources
+nano banana.yml
+# Paste yml from resources
+nano nexus/nexus.yml
+# Paste yml from resources
+
+nano ingress.yml
+# Paste yml from resources
+
+
+snap install microk8s --classic
+snap alias microk8s.kubectl kubectl
+microk8s.enable dns dashboard storage ingress metrics-server
+microk8s.stop
+microk8s.start
+#kubectl create -f dashboard-admin.yml
+
+# Access dashboard locally:
+kubectl proxy &
+# http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy
 
 # install cert-manager
 kubectl create namespace cert-manager
@@ -22,39 +50,23 @@ kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release
 ## Install cert-manager itself
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/cert-manager-no-webhook.yaml
 
-# install cert issuers
-mkdir cert_manager
-nano cert_manager/letsencryp_staging_issuer.yaml
-# Paste yml from resources
-nano cert_manager/selfsigned_issuer.yaml
-# Paste yml from resources
-nano cert_manager/ca_issuer.yaml
-# Paste yml from resources
 # TODO: tut mit rbac noch nicht
-kubectl apply -f cert_manager/letsencryp_staging_issuer.yaml
-kubectl apply -f cert_manager/selfsigned_issuer.yaml
-kubectl create secret tls ca-key-pair \
-   --cert=ca.crt --key=ca.key --namespace=default
+kubectl apply secret tls ca-key-pair \
+  --cert=ca.crt --key=ca.key --namespace=default
 kubectl apply -f cert_manager/ca_issuer.yaml
+# kubectl apply -f cert_manager/selfsigned_issuer.yaml
+# kubectl apply -f cert_manager/letsencryp_staging_issuer.yaml
+kubectl apply -f cert_manager/letsencryp_prod_issuer.yaml
 
 # apple & banana
-nano apple.yml
-# Paste yml from resources
-kubectl create -f apple.yml
-nano banana.yml
-# Paste yml from resources
-kubectl create -f banana.yml
+kubectl apply -f apple.yml
+kubectl apply -f banana.yml
 # nexus
-mkdir nexus
-nano nexus/nexus.yml
-# Paste yml from resources
-kubectl create -f nexus/nexus.yml
-nano ingress.yml
-# Paste yml from resources
-kubectl create -f ingress.yml
+kubectl apply -f nexus/nexus.yml
+# ingress
+kubectl apply -f ingress.yml
 
-# Insepct echo app at: https://[159.69.207.106]/apple
-
-# Access dashboard locally:
-kubectl proxy
-# http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy
+# Insepct echo app at:
+#     https://[159.69.207.106]/apple
+#     https://k8stest.domaindrivenarchitecture.org/apple
+#     https://k8stest.domaindrivenarchitecture.org/banana
