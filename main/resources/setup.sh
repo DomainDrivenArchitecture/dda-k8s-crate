b166ec6e66ba12dbc9d52b4acd942e31861951f3
hel <lucca.hellriegel@meissa-gmbh.de>
2019-03-12 14:01:03 +0100

    documented working sequence for RBAC activation

--- main/resources/setup.sh
+++ main/resources/setup.sh
@@ -4,23 +4,27 @@ scp -r main/resources `(whoami)`@159.69.207.106:
 
 # remote
 ssh `(whoami)`@159.69.207.106 -L 8001:localhost:8001
-rm -rf /tmp/resources
+sudo rm -rf /tmp/resources
 mv resources /tmp/
 
 sudo -i
 
 snap install microk8s --classic
 snap alias microk8s.kubectl kubectl
+# Wait a moment for snap to really finish
+microk8s.enable dns dashboard storage ingress metrics-server
+# Wait a minute as PODs are initializing -> all Status:Running with kubectl get pods --all-namespaces
+
+# RBAC
 nano /var/snap/microk8s/current/args/kube-apiserver
 #To activate RBAC replace "–authorization-mode=AlwaysAllow" with "–authorization-mode=RBAC" in line 5
-microk8s.enable dns dashboard storage ingress metrics-server
 microk8s.stop
 microk8s.start
-#kubectl create -f dashboard-admin.yml
 
-# Access dashboard locally:
-kubectl proxy
-# & http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy
+# ingress
+kubectl apply -f /tmp/resources/ingress.yml
+# dashboard
+kubectl apply -f /tmp/resources/dashboard.yml
 
 # clean up k8s setup?
 rm ca*
@@ -44,8 +48,6 @@ kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 ## Install cert-manager itself
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/cert-manager-no-webhook.yaml
 
-
-# TODO: tut mit rbac noch nicht
 openssl genrsa -out ca.key 2048
 openssl req -x509 -new -nodes -key ca.key -subj "/CN=domaindrivenarchitecture.org" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
 
@@ -66,10 +68,12 @@ kubectl apply -f /tmp/resources/cert_manager/letsencrypt_prod_issuer.yml
 # apple & banana
 kubectl apply -f /tmp/resources/apple.yml
 kubectl apply -f /tmp/resources/banana.yml
-# nexus
+# nexus, takes a few minutes to start
 kubectl apply -f /tmp/resources/nexus/nexus.yml
-# ingress
-kubectl apply -f /tmp/resources/ingress.yml
+
+# Access dashboard locally after waiting a few minutes:
+kubectl proxy &
+# http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy
 
 # Insepct echo app at:
 #     https://[159.69.207.106]/apple
