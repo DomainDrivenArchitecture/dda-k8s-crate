04bc2ed95dbcf3959bb4bf92a8eadbe107ffc7d6
hel <lucca.hellriegel@gmx.net>
2019-09-10 13:58:00 +0200

    deps update

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "2.2.2"]
+  :dependencies [[dda/dda-pallet "3.0.0"]
                  [dda/dda-serverspec-crate "1.3.4"]
                  [dda/dda-user-crate "2.0.4"]
-                 [dda/dda-config-commons "1.5.0"]
                  [dda/dda-git-crate "2.2.1"]]
   :target-path "target/%s/"
   :source-paths ["main/src"]
@@ -21,31 +20,28 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha3"]
-                    ;[expectations "2.2.0-SNAPSHOT"]
-                    [dda/pallet "0.9.0" :classifier "tests"]
+                   [[org.clojure/test.check "0.10.0-alpha4"]
+                    [dda/pallet "0.9.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
                    :plugins
-                   [[lein-sub "0.3.0"]
-                    [jonase/eastwood "0.3.4" :exclusions [org.clojure/clojure]]]
-                   :eastwood {:namespaces [:source-paths]}
+                   [[lein-sub "0.3.0"]]
                    :leiningen/reply
                    {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
                     :exclusions [commons-logging]}
-                   :repl-options {:init-ns dda.pallet.dda-smeagol-crate.app.instantiate-existing}}
+                   :repl-options {:init-ns dda.pallet.dda-k8s-crate.app.instantiate-existing}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.1" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
-                       :main dda.pallet.dda-smeagol-crate.main
+                       :main dda.pallet.dda-k8s-crate.main
                        :uberjar-name "dda-k8s-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "0.4.1"]
+                       :dependencies [[org.clojure/tools.cli "0.4.2"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
