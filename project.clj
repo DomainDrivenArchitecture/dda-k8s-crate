5abf0d90833eaad1ebb5990ee6b5784bfc52ffc4
jem <michael.jerger@meissa-gmbh.de>
2019-10-18 17:48:53 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[dda/dda-pallet "3.0.0"]
                  [dda/dda-serverspec-crate "1.3.4"]
-                 [dda/dda-user-crate "2.0.4"]
-                 [dda/dda-git-crate "2.2.1"]]
+                 [dda/dda-user-crate "2.0.5-SNAPSHOT"]
+                 [dda/dda-git-crate "2.2.2"]]
   :target-path "target/%s/"
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
@@ -20,10 +20,10 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha4"]
+                   [[org.clojure/test.check "0.10.0"]
                     [dda/pallet "0.9.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
+                    [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
                    :leiningen/reply
@@ -41,7 +41,7 @@
                        :dependencies [[org.clojure/tools.cli "0.4.2"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]}}
+                                      [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
