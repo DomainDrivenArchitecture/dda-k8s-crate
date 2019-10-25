b228d935a9a7efd99f011e1dc0a7518dd59c1e7c
jem <michael.jerger@meissa-gmbh.de>
2019-10-25 09:29:04 +0200

    refactor for less code

--- main/src/dda/pallet/dda_k8s_crate/infra/kubectl.clj
+++ main/src/dda/pallet/dda_k8s_crate/infra/kubectl.clj
@@ -117,8 +117,8 @@
      :group user
      :owner user
      :mode "755"
-     :content 
-     (selmer/render-file 
+     :content
+     (selmer/render-file
       (str path ".template") kubectl-config))))
 
 (defn user-configure-untaint-master
@@ -126,47 +126,51 @@
   (actions/as-action (logging/info (str facility " - system-install-k8s-base-config")))
   (actions/exec-checked-script
    "user-configure-untaint-master"
-   ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "taint" "nodes" "--all" "node-role.kubernetes.io/master-'")))
+   ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "taint" "nodes"
+           "--all" "node-role.kubernetes.io/master-'")))
 
-(defn kubectl-apply-f
+(s/defn kubectl-apply-f
   "apply kubectl config file"
-  [facility user path-on-server & [should-sleep?]]
-  (actions/as-action
-   (logging/info
-    (str facility "-install system: kubectl apply -f " path-on-server)))
-  (when should-sleep?
-    (actions/exec-checked-script "sleep" ("sleep" "180")))
-  (actions/exec-checked-script
-   "apply config file"
-   ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "apply" "-f" ~path-on-server "'")))
-
-(s/defn prepare-master-node
-  [apply-with-user
-   user-resource-path :- s/Str
+  [facility
    user :- s/Str
-   facility]
-  (apply-with-user (str user-resource-path "flannel/kube-flannel.yml"))
-  (apply-with-user (str user-resource-path "flannel/kube-flannel-rbac.yml"))
-  (user-configure-untaint-master facility user)
-  (apply-with-user (str user-resource-path "admin/admin_user.yml"))
-  (apply-with-user (str user-resource-path "dashboard/kubernetes-dashboard.yaml"))
-  (apply-with-user (str user-resource-path "metallb/metallb.yml"))
-  (apply-with-user (str user-resource-path "metallb/metallb_config.yml"))
-  (apply-with-user (str user-resource-path "ingress/mandatory.yaml"))
-  (apply-with-user (str user-resource-path "ingress/ingress_using_mettallb.yml")))
+   user-resource-path :- s/Str
+   resource :- s/Str
+   & [should-sleep?]]
+  (let [path-on-server (str user-resource-path resource)]
+    (actions/as-action
+     (logging/info (str facility " - kubectl-apply-f "
+                        path-on-server)))
+    (when should-sleep?
+      (actions/exec-checked-script "sleep" ("sleep" "180")))
+    (actions/exec-checked-script
+     (str "apply config " path-on-server)
+     ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "apply" "-f" ~path-on-server "'"))))
+
+(s/defn apply-networking
+  [apply-with-user]
+  (apply-with-user "flannel/kube-flannel.yml")
+  (apply-with-user "flannel/kube-flannel-rbac.yml"))
+
+(s/defn admin-dash-metal-ingress
+  [apply-with-user]
+  (apply-with-user "admin/admin_user.yml")
+  (apply-with-user "dashboard/kubernetes-dashboard.yaml")
+  (apply-with-user "metallb/metallb.yml")
+  (apply-with-user "metallb/metallb_config.yml")
+  (apply-with-user "ingress/mandatory.yaml")
+  (apply-with-user "ingress/ingress_using_mettallb.yml"))
 
 (s/defn install-cert-manager
   [apply-with-user
-   user-resource-path :- s/Str
-   user :- s/Str]
+   user :- s/Str
+   letsencrypt-prod :- s/Bool]
   (let [user-home-ca (str "home/" user "/ca")]
     (actions/exec-checked-script
      "create cert-manager ns"
-     ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "create" "namespace" "cert-manager'"))
-    (actions/exec-checked-script
-     "label cert-manager ns"
-     ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "label" "namespace" "cert-manager" "certmanager.k8s.io/disable-validation=true'"))
-    (apply-with-user (str user-resource-path "cert_manager/cert-manager.yaml"))
+     ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "create" "namespace" "cert-manager'")
+     ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "label" "namespace"
+             "cert-manager" "certmanager.k8s.io/disable-validation=true'"))
+    (apply-with-user "cert_manager/cert-manager.yaml")
     (actions/directory
      user-home-ca
      :owner user
@@ -174,59 +178,45 @@
      :mode "777")
     (actions/exec-checked-script
      "create cert key"
-     ("sudo" "-H" "-u" ~user "bash" "-c" "'openssl" "genrsa" "-out" ~(str user-home-ca "/ca.key") "2048'")
-     ("sudo" "-H" "-u" ~user "bash" "-c" "'openssl" "req" "-x509" "-new" "-nodes" "-key" ~(str user-home-ca "/ca.key") "-subj" "'/CN=test.domaindrivenarchitecture.org'"
-             "-days" "365" "-reqexts" "v3_req" "-extensions" "v3_ca" "-out" ~(str user-home-ca "/ca.crt") "'")
-     ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "create" "secret" "tls" "test-domaindrivenarchitecture-org-ca-key-pair"
+     ("sudo" "-H" "-u" ~user "bash" "-c" "'openssl" "genrsa" "-out"
+             ~(str user-home-ca "/ca.key") "2048'")
+     ("sudo" "-H" "-u" ~user "bash" "-c" "'openssl" "req" "-x509" "-new" "-nodes"
+             "-key" ~(str user-home-ca "/ca.key")
+             "-subj" "'/CN=test.domaindrivenarchitecture.org'"
+             "-days" "365" "-reqexts" "v3_req"
+             "-extensions" "v3_ca"
+             "-out" ~(str user-home-ca "/ca.crt") "'")
+     ("sudo" "-H" "-u" ~user "bash" "-c" "'kubectl" "create" "secret" "tls"
+             "test-domaindrivenarchitecture-org-ca-key-pair"
              ~(str "--cert=" user-home-ca "/ca.crt")
              ~(str "--key=" user-home-ca "/ca.key")
-             "--namespace=cert-manager'"))))
+             "--namespace=cert-manager'"))
+    (apply-with-user "cert_manager/letsencrypt_staging_issuer.yml" true)
+    (when letsencrypt-prod
+      (apply-with-user "cert_manager/letsencrypt_prod_issuer.yml" true))))
 
-;TODO: make optional
 (s/defn install-apple-banana
   [apply-with-user
-   user-resource-path :- s/Str]
-  (apply-with-user (str user-resource-path "apple_banana/apple.yml") true)
-  (apply-with-user (str user-resource-path "apple_banana/banana.yml")))
-
-;TODO: check if working
-(s/defn configure-ingress-and-cert-manager
-  [apply-with-user
-   user-resource-path :- s/Str
-   letsencrypt-prod]
-  (if letsencrypt-prod
-    (apply-with-user (str user-resource-path "apple_banana/ingress_simple_le_prod_https.yml"))
-    (apply-with-user (str user-resource-path "apple_banana/ingress_simple_le_staging_https.yml")))
-  (if letsencrypt-prod
-    (apply-with-user (str user-resource-path "cert_manager/letsencrypt_prod_issuer.yml") true)
-    (apply-with-user (str user-resource-path "cert_manager/letsencrypt_staging_issuer.yml") true)))
+   letsencrypt-prod :- s/Bool]
+  (apply-with-user "apple_banana/apple.yml" true)
+  (apply-with-user "apple_banana/banana.yml")
+  (apply-with-user "apple_banana/ingress_simple_le_staging_https.yml")
+  (when letsencrypt-prod
+    (apply-with-user "apple_banana/ingress_simple_le_prod_https.yml")))
 
 (s/defn install-nexus
   [apply-with-user
-   user-resource-path :- s/Str
-   user :- s/Str]
+   user :- s/Str
+   letsencrypt-prod :- s/Bool]
   (actions/directory
    "/mnt/data"
    :owner user
    :group user
    :mode "777")
-  (apply-with-user (str user-resource-path "nexus/nexus-storage.yml"))
-  (apply-with-user (str user-resource-path "nexus/nexus.yml"))
-  (apply-with-user (str user-resource-path "nexus/ingress_nexus_https.yml")))
-
-(s/defn kubectl-apply
-  "apply needed files and options"
-  [facility
-   user :- s/Str
-   config]
-  (let [{:keys [letsencrypt-prod]} config
-        apply-with-user (partial kubectl-apply-f facility user)
-        user-resource-path (str "/home/" user "/k8s_resources/")]
-    (prepare-master-node apply-with-user user-resource-path user facility)
-    (install-cert-manager apply-with-user user-resource-path user)
-    (install-apple-banana apply-with-user user-resource-path)
-    (configure-ingress-and-cert-manager apply-with-user user-resource-path letsencrypt-prod)
-    (install-nexus apply-with-user user-resource-path user)))
+  (apply-with-user "nexus/nexus-storage.yml")
+  (apply-with-user "nexus/nexus.yml")
+  ;TODO: switch for prod / staging
+  (apply-with-user "nexus/ingress_nexus_https.yml"))
 
 (s/defn init
   [facility
@@ -259,7 +249,18 @@
   [facility
    user :- s/Str
    config :- kubectl-config]
-  (actions/as-action (logging/info (str facility " - user-configure")))
+  (let [{:keys [letsencrypt-prod]} config
+        apply-with-user
+        (partial kubectl-apply-f facility user
+                 (str "/home/" user "/k8s_resources/"))]
+    (actions/as-action (logging/info (str facility " - user-configure")))
   ; TODO: run cleanup for being able do reaply config??
-  (user-configure-copy-yml facility user config)
-  (kubectl-apply facility user config))
+    (user-configure-copy-yml facility user config)
+
+    (apply-networking apply-with-user)
+    (user-configure-untaint-master facility user)
+    (admin-dash-metal-ingress apply-with-user)
+    (install-cert-manager apply-with-user user letsencrypt-prod)
+    ;TODO: make optional
+    (install-apple-banana apply-with-user letsencrypt-prod)
+    (install-nexus apply-with-user user letsencrypt-prod)))
