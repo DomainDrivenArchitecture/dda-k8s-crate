f0be8a4b1fc45ef54e5e3530b5b791ba0c6ba77c
jem <michael.jerger@meissa-gmbh.de>
2020-03-13 17:13:26 +0100

    refactor transport function

--- main/src/dda/pallet/dda_k8s_crate/infra/transport.clj
+++ main/src/dda/pallet/dda_k8s_crate/infra/transport.clj
@@ -15,6 +15,7 @@
 ; limitations under the License.
 (ns dda.pallet.dda-k8s-crate.infra.transport
   (:require
+   [clojure.string :as string]
    [clojure.tools.logging :as logging]
    [schema.core :as s]
    [pallet.actions :as actions]
@@ -41,3 +42,41 @@
      :mode "755"
      :content (selmer/render-file path {}))))
 
+(s/def Resource
+  {:filename s/Str
+   (s/optional-key :config) s/Any})
+
+(s/defn copy-resources-to-tmp
+  [facility :- s/Str
+   module :- s/Str
+   files :- [Resource]]
+  (let [facility-path (str "/tmp/" facility)
+        module-path (str facility-path "/" module)]
+    (actions/directory
+     facility-path
+     :group "root"
+     :owner "root")
+    (actions/directory
+     module-path
+     :group "root"
+     :owner "root")
+    (doseq [resource files]
+      (let [template? (contains? resource :config)
+            filename (:filename resource)
+            filename-on-target (str module-path "/" filename)
+            filename-on-source (if template?
+                                 (str module "/" filename ".template")
+                                 (str module "/" filename))
+            config (if template?
+                     (:config resource)
+                     {})
+            mode (if (string/ends-with? filename ".sh")
+                   "700"
+                   "600")]
+        (actions/remote-file
+         filename-on-target
+         :literal true
+         :group "root"
+         :owner "root"
+         :mode mode
+         :content (selmer/render-file filename-on-source config))))))
