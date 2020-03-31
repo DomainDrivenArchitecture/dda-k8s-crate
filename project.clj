a3e5a1e2fb44e82deabf898a6ebcdd432687e288
jem <michael.jerger@meissa-gmbh.de>
2020-03-31 17:28:14 +0200

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-k8s-crate "0.1.4-SNAPSHOT"
+(defproject dda/dda-k8s-crate "0.1.4"
   :description "Module for installing a server with kubeadm"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
