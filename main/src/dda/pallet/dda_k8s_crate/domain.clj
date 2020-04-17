c58c875b5c34760b42d2aa7bc0a5ccc242461a04
jem <michael.jerger@meissa-gmbh.de>
2020-04-17 16:23:05 +0200

    format

--- main/src/dda/pallet/dda_k8s_crate/domain.clj
+++ main/src/dda/pallet/dda_k8s_crate/domain.clj
@@ -25,7 +25,8 @@
 
 (s/def k8sDomain
   {:user s/Keyword
-   :k8s {:external-ip s/Str (s/optional-key :external-ipv6) s/Str (s/optional-key :advertise-address) s/Str}
+   :k8s {:external-ip s/Str (s/optional-key :external-ipv6) s/Str 
+         (s/optional-key :advertise-address) s/Str}
    :cert-manager (s/enum :letsencrypt-prod-issuer :letsencrypt-staging-issuer :selfsigned-issuer)
    (s/optional-key :apple) {:fqdn s/Str}
    (s/optional-key :nexus) {:fqdn s/Str}})
