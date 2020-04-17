e41d83d2350f636504edb206d15aa6c7f25c803e
jem <michael.jerger@meissa-gmbh.de>
2020-04-17 20:27:57 +0200

    added some more verification

--- main/resources/cert-manager/verify.sh
+++ main/resources/cert-manager/verify.sh
@@ -4,3 +4,10 @@ echo -e "\n====================\n"
 echo -e "expect some pods running"
 echo -e "\n====================\n"
 kubectl get all --namespace cert-manager
+
+echo -e "\n====================\n"
+echo -e "expect some cert (see also https://cert-manager.io/docs/faq/acme/ ) \n"
+echo -e "  -> kubectl describe certificaterequest <CERT_REQUEST_FROM_ABOVE> \n"
+echo -e "  -> kubectl describe order <ORDER_FROM_ABOVE> \n"
+echo -e "\n====================\n"
+kubectl describe certificate jira-neu-prod-meissa-gmbh-de
